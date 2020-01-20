62d0481c646b982cccc97dcbe14668a2e9f0a25a
Muhammad Umer <mumer271992@gmail.com>
2020-01-21 02:23:52 +0500

    Add Plant test fixed

--- src/components/AddPlant/AddPlant.test.js
+++ src/components/AddPlant/AddPlant.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { mount } from 'enzyme';
 import axios from 'axios';
+import { mount } from 'enzyme';
 
 import AddPlant from './AddPlant';
 import { StateProvider } from '../../store/store';
@@ -28,44 +28,49 @@ describe('renders add plant component without crashing', () => {
     expect(wrapper.length).toEqual(1);
   });
 
-  it('is form fields rendered', () => {
+  it('should render form fields', () => {
     const inputWrapper = wrapper.find('[data-test="plant-name"]');
     const buttonWrapper = wrapper.find('[data-test="add-plant-btn"]');
     expect(inputWrapper.length).toEqual(1);
     expect(buttonWrapper.length).toEqual(1);
   });
 
-  it('submit empty form and show error', () => {
+  it('should display error on submiting empty form', () => {
     const formWrapper = wrapper.find('[data-test="add-plant-form"]').at(0);
     formWrapper.simulate('submit');
     const errorAlertWrapper = wrapper.find('[data-test="danger-alert"]');
     expect(errorAlertWrapper.length).toEqual(1);
   });
 
-  it('fill and submit form', () => {
-    const inputWrapper = wrapper.find('[data-test="plant-name"]').at(0);
-    const formWrapper = wrapper.find('[data-test="add-plant-form"]').at(0);
+  it('should display success message on submit form', () => {
     const plantName = 'Test plant';
-    inputWrapper.props().value = plantName;
-    inputWrapper.simulate('change', {
-      target: {
-        value: plantName,
-        name: 'plant-name'
-      }
-    });
-    expect(inputWrapper.props().value).toEqual(plantName);
-    axios.post.mockImplementationOnce(() =>
+    wrapper
+      .find('[data-test="plant-name"]')
+      .at(0)
+      .simulate('change', {
+        target: {
+          value: plantName,
+          name: 'plant-name'
+        }
+      });
+    expect(
+      wrapper
+        .find('[data-test="plant-name"]')
+        .at(0)
+        .props().value
+    ).toEqual(plantName);
+    const mockedFunc = jest.fn(() =>
       Promise.resolve({
         uid: 'aasdasdads',
         name: plantName
       })
     );
+    axios.post = mockedFunc;
 
-    formWrapper.simulate('submit');
-    setTimeout(() => {
-      expect(inputWrapper.props().value).toEqual('');
-      const successAlertWrapper = wrapper.find('[data-test="success-alert"]');
-      expect(successAlertWrapper).toEqual(1);
-    }, 1000);
+    wrapper
+      .find('[data-test="add-plant-form"]')
+      .at(0)
+      .simulate('submit');
+    expect(mockedFunc).toHaveBeenCalled();
   });
 });
